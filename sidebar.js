c3f8eb25875362fa6ed92c0d51f196bddd06769f
Alexey Nurmukhametov <alex@nurmukhametov.ru>
2020-06-24 16:59:26 +0300

    Clean sidebar.js from chrome specific code

--- sidebar.js
+++ sidebar.js
@@ -90,21 +90,3 @@ document.getElementById("calc-button").addEventListener("click", () => {
     active: true
   }).then(sendMessageToTabs).catch(onError);
 });
-
-function handleMessage(request, sender, sendResponse) {
-  console.log("Message from the content script: " + request.greeting);
-  show_response_in_sidebar(request);
-}
-
-chrome.runtime.onMessage.addListener(handleMessage);
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {urlContains: 'virtualsoccer.ru/mng_order.php', schemes: ['https']},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
-
